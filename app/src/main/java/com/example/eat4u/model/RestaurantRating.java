d60e1c1389d0edc28a1089c77e0445204b6eb110
Houssem Nasri <housi.housi2015@gmail.com>
2023-03-18 02:07:56 +0100

    Populate RestaurantRating class

--- app/src/main/java/com/example/eat4u/model/RestaurantRating.java
+++ app/src/main/java/com/example/eat4u/model/RestaurantRating.java
@@ -1,9 +1,38 @@
 package com.example.eat4u.model;
 
 public class RestaurantRating {
-    private Quality foodQuality;
-    private Quality serviceQuality;
-    private Stars stars;
-    private Double averagePrice;
-    private User rater;
+    private final Quality foodQuality;
+    private final Quality serviceQuality;
+    private final Stars stars;
+    private final Double averagePrice;
+    private final User rater;
+
+
+    public RestaurantRating(Quality foodQuality, Quality serviceQuality, Stars stars, Double averagePrice, User rater) {
+        this.foodQuality = foodQuality;
+        this.serviceQuality = serviceQuality;
+        this.stars = stars;
+        this.averagePrice = averagePrice;
+        this.rater = rater;
+    }
+
+    public Quality getFoodQuality() {
+        return foodQuality;
+    }
+
+    public Quality getServiceQuality() {
+        return serviceQuality;
+    }
+
+    public Stars getStars() {
+        return stars;
+    }
+
+    public Double getAveragePrice() {
+        return averagePrice;
+    }
+
+    public User getRater() {
+        return rater;
+    }
 }
