836464b5e4f97863a54b06e5d58c1254e75b94f4
Houssem Nasri <housi.housi2015@gmail.com>
2023-03-18 02:07:42 +0100

    Populate user class

--- app/src/main/java/com/example/eat4u/model/User.java
+++ app/src/main/java/com/example/eat4u/model/User.java
@@ -1,4 +1,37 @@
 package com.example.eat4u.model;
 
 public class User {
+    private final Long id;
+    private final String firstname;
+    private final String lastname;
+    private final String email;
+    private final String phone;
+
+    public User(Long id, String firstname, String lastname, String email, String phone) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
