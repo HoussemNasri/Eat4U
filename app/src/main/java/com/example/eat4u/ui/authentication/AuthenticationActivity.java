5109f24ab6459f4f5d164554e32cf1e4dc77c4b8
Houssem Nasri <housi.housi2015@gmail.com>
2023-04-08 00:34:19 +0100

    Validate stuff

--- app/src/main/java/com/example/eat4u/ui/authentication/AuthenticationActivity.java
+++ app/src/main/java/com/example/eat4u/ui/authentication/AuthenticationActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.eat4u.R;
 import com.example.eat4u.ui.review_editor.ReviewEditorActivity;
 import com.example.eat4u.utils.Globals;
+import com.example.eat4u.utils.StringUtils;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.stream.Stream;
 
 public class AuthenticationActivity extends AppCompatActivity {
     private TextInputEditText firstnameEditText;
@@ -26,10 +30,16 @@ public class AuthenticationActivity extends AppCompatActivity {
     }
 
     public void onLoginClicked(View view) {
-        Intent goToReviewEditorIntent = new Intent(this, ReviewEditorActivity.class);
-        goToReviewEditorIntent.putExtra(Globals.USER_FIRSTNAME_EXTRA, firstnameEditText.getText().toString());
-        goToReviewEditorIntent.putExtra(Globals.USER_LASTNAME_EXTRA, lastnameEditText.getText().toString());
-        finish();
-        startActivity(goToReviewEditorIntent);
+        boolean isValidInput = Stream.of(firstnameEditText.getText().toString(), lastnameEditText.getText().toString())
+                .allMatch(StringUtils::isNotBlank);
+        if (isValidInput) {
+            Toast.makeText(this, "Invalid Stuff!", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent goToReviewEditorIntent = new Intent(this, ReviewEditorActivity.class);
+            goToReviewEditorIntent.putExtra(Globals.USER_FIRSTNAME_EXTRA, firstnameEditText.getText().toString());
+            goToReviewEditorIntent.putExtra(Globals.USER_LASTNAME_EXTRA, lastnameEditText.getText().toString());
+            finish();
+            startActivity(goToReviewEditorIntent);
+        }
     }
 }
